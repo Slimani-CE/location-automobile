36557ca67b32b40bb680789b5543034a5a650a9f
Mustapha SLIMANI <itsmustaphahere@gmail.com>
2022-03-31 18:02:28 +0000

    Add files via upload

---
+++ Script/updates.js
@@ -0,0 +1,9 @@
+function openUpdates(){
+    updatesSection = document.getElementsByClassName("updates-container")[0];
+    updatesSection.style="display:flex;";   
+}
+
+function closeUpdates(closBtn){
+    closBtn.parentNode.parentNode.parentNode.style="display:none";
+}
+
